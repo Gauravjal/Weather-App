06986e9a6cbbf033eeb8397892b5925397b68940
Gaurav Jalkote <gauravjalkote012@gmail.com>
2023-01-23 16:32:29 +0530

    City doesn't exist bug fixed

--- src/App.js
+++ src/App.js
@@ -7,30 +7,41 @@ import Info from './components/Info.jsx'
 import Forecast from './components/Forecast.jsx'
 import getWeatherData from './services/weatherService.js';
 import CityContext from './components/context/CityContext'
+import NotFound from './components/NotFound'
 function App() {
   const [city,setCity]=useState("latur");
   const [weatherData,setWeatherData]=useState(null);
   useEffect(()=>{
   const fetchWeather=async()=>{
     const data=await getWeatherData("weather",{q:city})
-    console.log("data",data);
+    console.log(data);
     setWeatherData(data);
+    
+    // console.log("data",data);
+    // setWeatherData(data);
   }
   fetchWeather();
 },[city])
   return (
+    
     <CityContext.Provider value={{city,setCity}}>
+      {weatherData && 
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-10 bg-cover bg-blue-600">
-        <Heading weatherData={weatherData}/>
-        <Inputs city={city}/>
+    { weatherData.hasOwnProperty("message") ||<Heading weatherData={weatherData}/>}
+
+   
+    <Inputs city={city}/>
         {/* <Local weatherData={weatherData}/>
         {console.log("check",weatherData)} */}
-        <Info weatherData={weatherData}/>
+         {weatherData.hasOwnProperty("message") || <Info weatherData={weatherData}/>}
         {/* {console.log("this one",weatherData.name)} */}
-        {weatherData && <Forecast weatherData={weatherData}/>}
+        {weatherData.hasOwnProperty("message") || <Forecast weatherData={weatherData}/>}
         {/* <Forecast weatherData={weatherData}/> */}
+        {weatherData.hasOwnProperty("message") && <NotFound/>}
     </div>
+     }
     </CityContext.Provider>
+     
   );
 }
 
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='text-white text-center'><h1>The city doesn't exist !!</h1>
+    (Make sure that the city name is correct)
+    </div>
+  )
+}
+
+export default NotFound
